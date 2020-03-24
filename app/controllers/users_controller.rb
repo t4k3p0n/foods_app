844a695e33b235fcbcdff28fc7fe513e79122595
kanwatanabe <kkk.k10@icloud.com>
2020-03-25 00:12:21 +0900

    before_action エラーの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
                                                  :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
 
-  before_action :logged_in_user, only: [index edit update destroy
-                                          following followers]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                          :following, :followers]
   
   before_action :admin_user,     only: :destroy
 
